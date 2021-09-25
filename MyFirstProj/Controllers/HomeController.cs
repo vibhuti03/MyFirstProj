63c8676983407adca835d3f57386fb73e908075d
vibhuti03 <vibhutimaheshwari03@gmail.com>
2021-09-25 21:30:31 +0530

    Attribut Based Routing - Tokens

--- MyFirstProj/Controllers/HomeController.cs
+++ MyFirstProj/Controllers/HomeController.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace MyFirstProj.Controllers
 {
-    [Route("Home")]
+    [Route("[controller]")]
     public class HomeController : Controller
     {
 
-        [Route("Index")]
+        [Route("[action]")]
         [Route("~/")]       //for default path to also point to Index()
         public IActionResult Index() //action method with name Index
         {
             return View();
         }
 
-        [Route("About")]
+        [Route("[action]")]
         public IActionResult About() //action method with name Index
         {
             return View();
         }
 
-        [Route("Details/{id?}")]
+        [Route("[action]/{id?}")]
         public int Details(int? id)
         {
             return id??1;   //null coalescing operator
