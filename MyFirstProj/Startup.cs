27d2e882b1e016757547c901076421802b8a0ec1
vibhuti03 <vibhutimaheshwari03@gmail.com>
2021-09-25 08:36:20 +0530

    Basic Convention Routing -> UseMvcWithDefaultRoute()

--- MyFirstProj/Startup.cs
+++ MyFirstProj/Startup.cs
@@ -15,37 +15,24 @@ namespace MyFirstProj
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //To get functionality of MVC
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //default middleware    -   MIDDLEWARE 1
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            //Middleware defined    -   MIDDLEWARE 2
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("First Middleware executed...\n");
-                await next();
-            });
+            //Convention Routing
+            //URL -> http://localhost:84390//Home//Index
+            //    -> http://localhost:84390//Home//About 
+            app.UseMvcWithDefaultRoute(); //Home Controller -> Index Action Method
 
-            //Middleware defined    -   MIDDLEWARE 2
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Second Middleware executed...\n");
-                await next();
-            });
-
-            //default middleware whose response is "Hello World!"
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!\n");
-            });
         }
     }
 }
