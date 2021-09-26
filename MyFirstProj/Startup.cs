fb1fcf6374ada69f66239622f3c5928754c3b381
vibhuti03 <vibhutimaheshwari03@gmail.com>
2021-09-26 21:24:48 +0530

    Including ViewStart file to decrease redundancy

--- MyFirstProj/Startup.cs
+++ MyFirstProj/Startup.cs
@@ -28,10 +28,14 @@ namespace MyFirstProj
                 app.UseDeveloperExceptionPage();
             }
 
-          app.UseMvc(routes =>
-          {
-              routes.MapRoute("default", "{controller=Home}/{action=Index}");
-          });
+            app.UseMvc();
+
+          //app.UseMvc(routes =>
+          //{
+          //    routes.MapRoute(
+          //        name:"default", 
+          //        template: "{controller=Home}/{action=Index}");
+          //});
 
             app.UseStaticFiles();
         }
